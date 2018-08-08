3cb8d91952b191318a6fabd71e2c0b2cf7b8d5e7
siva peddiboyena <siva.peddiboyena@nexiilabs.com>
2018-08-08 20:24:51 +0530

    Fourth commit

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,13 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
-  
-  /**
-   * 
+  /** 
    * @param someone the name of the person
    * @return greeting string
-   */
-  
+   */  
   public String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
